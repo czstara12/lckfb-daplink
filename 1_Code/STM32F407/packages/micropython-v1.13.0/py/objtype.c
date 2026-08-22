fc49478d13199099d009c9486ae8fa579b02bf6d
starstory <czstara12@gmail.com>
2026-08-22 12:03:27 +0800

    fix(micropython): 修复 qstr 编号溢出

--- 1_Code/STM32F407/packages/micropython-v1.13.0/py/objtype.c
+++ 1_Code/STM32F407/packages/micropython-v1.13.0/py/objtype.c
@@ -366,9 +366,7 @@ mp_obj_t mp_obj_instance_make_new(const mp_obj_type_t *self, size_t n_args, size
     return MP_OBJ_FROM_PTR(o);
 }
 
-// Qstrs for special methods are guaranteed to have a small value, so we use byte
-// type to represent them.
-const byte mp_unary_op_method_name[MP_UNARY_OP_NUM_RUNTIME] = {
+const qstr mp_unary_op_method_name[MP_UNARY_OP_NUM_RUNTIME] = {
     [MP_UNARY_OP_BOOL] = MP_QSTR___bool__,
     [MP_UNARY_OP_LEN] = MP_QSTR___len__,
     [MP_UNARY_OP_HASH] = MP_QSTR___hash__,
@@ -458,9 +456,7 @@ STATIC mp_obj_t instance_unary_op(mp_unary_op_t op, mp_obj_t self_in) {
 // Binary-op enum values not listed here will have the default value of 0 in the
 // table, corresponding to MP_QSTRnull, and are therefore unsupported (a lookup will
 // fail).  They can be added at the expense of code size for the qstr.
-// Qstrs for special methods are guaranteed to have a small value, so we use byte
-// type to represent them.
-const byte mp_binary_op_method_name[MP_BINARY_OP_NUM_RUNTIME] = {
+const qstr mp_binary_op_method_name[MP_BINARY_OP_NUM_RUNTIME] = {
     [MP_BINARY_OP_LESS] = MP_QSTR___lt__,
     [MP_BINARY_OP_MORE] = MP_QSTR___gt__,
     [MP_BINARY_OP_EQUAL] = MP_QSTR___eq__,
